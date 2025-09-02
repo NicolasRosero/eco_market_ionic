2767f0a6ee64dad743f6ced2bc27230ff040c4d6
Nicolas Rosero <nicolass2310@gmail.com>
2025-09-01 20:49:40 -0500

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eco_market_ionic',
+  webDir: 'www'
+};
+
+export default config;
