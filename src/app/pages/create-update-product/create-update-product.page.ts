8807ed9ff7d1374b6339b53220d5bc6ba583a247
Sebastian Paez Prado <sebastianpaez@ucompensar.edu.co>
2025-11-04 20:10:00 -0500

    Ajustes melos en gestión de productos

--- src/app/pages/create-update-product/create-update-product.page.ts
+++ src/app/pages/create-update-product/create-update-product.page.ts
@@ -1,35 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HeaderComponent } from "src/app/components/header/header.component";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
+import { Category, Product } from 'src/app/types';
 
 @Component({
   selector: 'app-create-update-product',
   templateUrl: './create-update-product.page.html',
   styleUrls: ['./create-update-product.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, ReactiveFormsModule, HeaderComponent]
+  imports: [IonicModule, CommonModule, ReactiveFormsModule, HeaderComponent],
 })
 export class CreateUpdateProductPage implements OnInit {
+  productForm: FormGroup;
+  isEditing: boolean = false;
+  categories: Category[] = [];
 
-productForm: FormGroup
-
-  constructor( 
-    
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-
+    private productService: ProductsService
   ) {
     this.productForm = formBuilder.group({
       name: ['', Validators.required],
       description: ['', [Validators.required]],
-      Precio: [null, [Validators.required]],
+      precio: [null, [Validators.required]],
       category: ['', Validators.required],
       stock: ['', [Validators.required]],
       noted: ['', Validators.required],
-      agreeTerms: [false]
     });
-   }
 
-  ngOnInit() { }
+    this.activeRoute.paramMap.subscribe((params: any) => {
+      const id = params.get('id') || null;
+
+      if (id) {
+        this.isEditing = true;
+        this.getProductById(parseInt(id));
+      }
+    });
+  }
+
+  async ngOnInit() {
+    this.getCategories();
+  }
+
+  async getCategories(): Promise<void> {
+    this.categories = await this.productService.getCategories();
+  }
+
+  async getProductById(productId: number): Promise<void> {
+    const product: Product | undefined = await this.productService.getProductById(productId);
+
+    if(product) {
+      this.productForm.controls['name'].setValue(product.name);
+      this.productForm.controls["description"].setValue(product.desc);
+      this.productForm.controls['precio'].setValue(product.price);
+      this.productForm.controls['stock'].setValue(product.stock);
+    }
+  }
+
+  async saveData(): Promise<void> {
+    if (this.isEditing) {
+      // Editar
+    } else {
+      // Crear
+    }
+  }
 }
